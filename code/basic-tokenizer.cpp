d29278e32e26cfc7fbd7c5ff4bf8dec91e07ebf9
Justin Heyes-Jones <justinhj@gmail.com>
2024-03-30 19:52:27 -0700

    not much

--- code/basic-tokenizer.cpp
+++ code/basic-tokenizer.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[]) {
   const u8string test_strings[] = {
     u8"", // empty string
     u8"?", // single character
-    /* "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.", */
-    u8"안 👋 hello", // from the youtube video
+    u8"Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.", // From Kaparthy's youtube video
+    u8"안 👋 hello", // also from the youtube video
     u8"hello world!!!? (안녕하세요!) lol123 😉", // fun small string
     u8"FILE:taylorswift.txt", // FILE: is handled as a special string in unpack() TODO
   };
