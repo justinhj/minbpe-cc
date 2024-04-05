a66d3875ab983820d4f47291e817a538a5883105
Justin Heyes-Jones <justinhj@gmail.com>
2024-04-04 22:29:45 -0700

    tokenization matches python now

--- code/BasicTokenizer.hpp
+++ code/BasicTokenizer.hpp
@@ -2,12 +2,16 @@
 #include <climits>
 #include <iostream>
 #include <ranges>
+#include <tuple>
 #include <vector>
+#include <algorithm>
 
 using std::string;
 using std::unordered_map;
 using std::cout;
 using std::endl;
+using std::tuple;
+using std::make_tuple;
 
 class BasicTokenizer : public Tokenizer {
   public:
@@ -20,39 +24,58 @@ class BasicTokenizer : public Tokenizer {
 
   protected:
   // Count the frequencies of all pairs and return the most frequently occuring
-  std::tuple<int,int> most_frequent_pair(const std::vector<int> &text) {
+  tuple<int,int,int> most_frequent_pair(const std::vector<int> &text) {
     assert(text.size() > 1);
     if(text.size() == 2) {
-      return std::make_tuple(text[0], text[1]);
+      return make_tuple(text[0], text[1], 1);
     }
     // Would like to use a zip iterator here but doesn't seem to be support in Apple Clang yet
-    /* for(std::tuple<int,int> elem: std::views::zip(text, &text[1])) { } */
+    /* for(tuple<int,int> elem: std::views::zip(text, &text[1])) { } */
     auto i1 = text.begin();
     auto i2 = ++text.begin();
-    unordered_map<merge_key_t, int, decltype(key_hash_lambda)> freqs(10, key_hash_lambda);
-    int max = 0;
-    std::tuple<int,int> max_pair;
+    auto step = 0; // count the steps so we can pick the first added to the map
+    // this is to work around unordered map not having insertion order maintained
+    unordered_map<merge_key_t, tuple<int,int>, decltype(key_hash_lambda)> freqs(10, key_hash_lambda);
     while(i1 != text.end() && i2 != text.end()) {
-      auto p = std::make_tuple(*i1, *i2);
+      auto p = make_tuple(*i1, *i2);
       int f = 1;
       if(freqs.find(p) == freqs.end()) {
-        freqs[p] = f;
+        freqs[p] = make_tuple(f,step);
       } else {
-        freqs[p]++;
-        f = freqs[p];
-      }
-      if(f >= max) {
-        max = f;
-        max_pair = p;
+        auto tf = freqs[p];
+        f = std::get<0>(freqs[p]);
+        auto s = std::get<1>(freqs[p]);
+        freqs[p] = make_tuple(++f,s);
       }
       ++i1;
       ++i2;
+      ++step;
     }
-    return max_pair;
+    auto maxElementIt = std::max_element(freqs.begin(), freqs.end(),
+        [](const std::pair<const tuple<int,int>, tuple<int,int>>& a, const std::pair<const tuple<int,int>, tuple<int,int>>& b) -> bool {
+            auto av = std::get<0>(a.second); 
+            auto as = std::get<1>(a.second); 
+            auto bv = std::get<0>(b.second); 
+            auto bs = std::get<1>(b.second); 
+            if(av < bv) {
+              return true;
+            } else if (av == bv) {
+              if(bs < as) {
+                return true;
+              } else {
+                return false;
+              }
+            } else {
+              return false;
+            }
+        });
+    auto max_pair = *maxElementIt;
+    auto return_pair = max_pair.first;
+    auto max = std::get<0>(max_pair.second);
+    return make_tuple(std::get<0>(return_pair), std::get<1>(return_pair), max);
   }
-
   // replace all consecutive occurences of pair with the new token idx
-  void merge(std::vector<int> &text, std::tuple<int,int> mp, int idx) {
+  void merge(std::vector<int> &text, tuple<int,int> mp, int idx) {
     if(text.size() < 2) {
       return;
     }
@@ -69,7 +92,7 @@ class BasicTokenizer : public Tokenizer {
         i2++;
         continue;
       }
-      auto p = std::make_tuple(*i1,*i2);
+      auto p = make_tuple(*i1,*i2);
       if(p == mp) {
         skip = true;
         new_text.push_back(idx);
@@ -99,7 +122,7 @@ class BasicTokenizer : public Tokenizer {
     auto merge_count = 0;
     auto len = text.size();
     while(i < len) {
-      auto pair = std::make_tuple(text[i], text[i + 1]);
+      auto pair = make_tuple(text[i], text[i + 1]);
       if(i < len - 1 && merges.find(pair) != merges.end()) {
         out.push_back(merges[pair]);
         i += 2;
@@ -114,24 +137,6 @@ class BasicTokenizer : public Tokenizer {
     } else {
       return internal_encode(out, verbose);
     }
-/* def encode(ids): */
-/*     out = [] */
-/*     i = 0 */
-/*     merge_count = 0 */
-/*     while i < len(ids): */
-/*         # 0 1 2 3 */
-/*         # a b c d */
-/*         if i < len(ids) - 1 and (ids[i], ids[i + 1]) in merges: */
-/*             out.append(merges[(ids[i], ids[i + 1])]) */
-/*             i += 2; */
-/*             merge_count += 1 */
-/*         else: */
-/*             out.append(ids[i]) */
-/*             i += 1; */
-/*     if merge_count == 0: */
-/*         return out */
-/*     else: */
-/*         return encode(out) */
   }
   public:
   // Warning for compatibility with common tokenizers it is assumed the input is in 
@@ -145,9 +150,6 @@ class BasicTokenizer : public Tokenizer {
     /*   cout << "\n"; */
     /* } */
     auto text_converted = text_to_vector(text);
-    for(auto c : text) {
-      text_converted.push_back(static_cast<int>(char_to_int(c)));
-    }
     vocab.clear();
     for(auto i=0; i<(UCHAR_MAX + 1); i++) {
       vector<int> s;
@@ -163,10 +165,11 @@ class BasicTokenizer : public Tokenizer {
       /*   cout << c <<  ' '; */
       /* } */
       /* cout << "\n"; */
-      auto mp = most_frequent_pair(text_converted);
+      auto mp3 = most_frequent_pair(text_converted);
       if(verbose) {
-        cout << "merge pair " << std::get<0>(mp) << ", " << std::get<1>(mp) << " with new token " << i << "\n";
+        cout << "merge pair " << std::get<0>(mp3) << ", " << std::get<1>(mp3) << " with new token " << i << " count " << std::get<2>(mp3) << "\n";
       }
+      auto mp = make_tuple(std::get<0>(mp3), std::get<1>(mp3));
       merge(text_converted, mp, i);
       merges[mp] = i;
 
--- code/basic-tokenizer.cpp
+++ code/basic-tokenizer.cpp
@@ -1,33 +1,60 @@
 #include "BasicTokenizer.hpp"
-
+#include <fstream>
+#include <iostream>
 using std::string;
 
 int char_to_int(char8_t c) {
   return c < 0 ? c + 256 : c; 
 }
+const string test_strings[] = {
+  "abab",
+  "FILE:tests/sample.txt",
+  "FILE:tests/taylorswift.txt",
+  "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.",
+  "", // empty string
+  "?", // single character
+  "안 👋 hello", // also from the youtube video
+  "hello world!!!? (안녕하세요!) lol123 😉", // fun small string
+  "FILE:taylorswift.txt", // FILE: is handled as a special string in unpack() TODO
+  "But Unicode can be abstruse plus we know we out to be still finidng the whole thing mysterious",
+};
+string getTestString(int index) {
+  if (index < 0 || index >= sizeof(test_strings) / sizeof(test_strings[0])) {
+    std::cerr << "Index out of bounds." << std::endl;
+    return "";
+  }
+  string str = test_strings[index];
+  const string fileIndicator = "FILE:";
+  if (str.find(fileIndicator) != str.npos) {
+    std::string file_path = str.substr(fileIndicator.length());
+    std::ifstream file(file_path);
+    std::string file_content;
+
+    if (file) {
+      file_content.assign((std::istreambuf_iterator<char>(file)),
+                          std::istreambuf_iterator<char>());
+      file.close();
+      return file_content;
+    } else {
+      std::cerr << "Could not open file: " << file_path << std::endl;
+      return "";
+    }
+  }
+  cout << str << " does not begin with " << fileIndicator << "\n";
+  return str;
+}
 // Take the BasicTokenizer for a test drive
 int main(int argc, char *argv[]) {
-
-  const string test_strings[] = {
-    "ababcdcdcdabcdef",
-    "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.", // From Kaparthy's youtube video
-    "", // empty string
-    "?", // single character
-    "안 👋 hello", // also from the youtube video
-    "hello world!!!? (안녕하세요!) lol123 😉", // fun small string
-    "FILE:taylorswift.txt", // FILE: is handled as a special string in unpack() TODO
-    "But Unicode can be abstruse plus we know we out to be still finidng the whole thing mysterious",
-  };
-
   auto verbose = true;
+  auto input = getTestString(3);
 
   BasicTokenizer bt;
-  bt.train(test_strings[1], 256 + 20, verbose);
-  auto encoded = bt.encode(test_strings[1], verbose);
-  cout << "Original string length " << test_strings[1].size() << "\n";
+  bt.train(input, 256 + 20, verbose);
+  auto encoded = bt.encode(input, verbose);
+  cout << "Original string length " << input.size() << "\n";
   cout << "Encoded string length " << encoded.size() << "\n";
   auto decoded = bt.decode(encoded, false);
 
-  cout << "Original string -- " << test_strings[1] << "\n";
-  cout << "Decoded string  -- " << decoded << "\n";
+  /* cout << "Original string -- " << input << "\n"; */
+  /* cout << "Decoded string  -- " << decoded << "\n"; */
 }
