129a0e02a0ff00f0abb6f6f2f9074ebcd713e41e
Justin Heyes-Jones <justinhj@gmail.com>
2024-04-19 16:44:46 -0700

    start on cli

--- code/examples/minbpe-cc.cpp
+++ code/examples/minbpe-cc.cpp
@@ -7,91 +7,47 @@
 using std::string;
 using std::optional;
 
-// TODO can get rid of these once the full CLI interface is working
-const string test_strings[] = {
-  "with 20 and 201 and 2001 what is happen?",
-  "FILE:data/sample.txt",
-  "FILE:data/taylorswift.txt",
-  "FILE:data/shakespeare.txt",
-  "FILE:data/bible.txt",
-  "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.",
-  "", // empty string
-  "?", // single character
-  "안 👋 hello", // also from the youtube video
-  "hello world!!!? (안녕하세요!) lol123 😉", // fun small string
-  "But Unicode can be abstruse plus we know we out to be still finidng the whole thing mysterious",
-};
-
-string getTestString(int index) {
-  if (index < 0 || index >= sizeof(test_strings) / sizeof(test_strings[0])) {
-    std::cerr << "Index out of bounds." << std::endl;
-    return "";
-  }
-  string str = test_strings[index];
-  const string fileIndicator = "FILE:";
-  if (str.find(fileIndicator) != str.npos) {
-    std::string file_path = str.substr(fileIndicator.length());
-    std::ifstream file(file_path);
-    std::string file_content;
-
-    if (file) {
-      cout << "Loading input file: " << file_path << "\n";
-      file_content.assign((std::istreambuf_iterator<char>(file)),
-                          std::istreambuf_iterator<char>());
-      file.close();
-      return file_content;
-    } else {
-      std::cerr << "Could not open file: " << file_path << std::endl;
-      return "";
-    }
-  }
-  cout << str << " does not begin with " << fileIndicator << "\n";
-  return str;
-}
-// Take the BasicTokenizer for a test drive
+// Command line training, encoding and decoding
 int main(int argc, char *argv[]) {
-  CLI::App app{"App description"};
+  CLI::App app{"Training, encoding and decoding of tokens"};
   argv = app.ensure_utf8(argv);
 
-  optional<string> filename;
-  app.add_option("-f,--file", filename, "A help string");
+  optional<string> input_path;
+  app.add_option("-i,--input", input_path, "Input file path");
+
+  bool train;
+  app.add_flag("-t,--train", train, "Train on the input");
 
   CLI11_PARSE(app, argc, argv);
 
-  if(filename.has_value()) {
-    cout << "filename " << filename.value() << "\n";
+  if(input_path.has_value()) {
+    cout << "Input file " << input_path.value() << "\n";
+  } else {
+    cout << "Input file <stdin>\n";
   }
 
-    const string text = R"(RCA Republic Big Machine
-Website	www.taylorswift.com Edit this at Wikidata
-Signature
-Taylor Alison Swift (born December 13, 1989) is an American singer-songwriter. Her versatile artistry, songwriting, and entrepreneurship have influenced the music industry, popular culture, and politics, and her life is a subject of widespread media coverage.
-안 👋 hello
-)";
-
-  /* RegexTokenizer rt = RegexTokenizer(); */
-  /* RegexTokenizer rt = RegexTokenizer(RegexTokenizer::GPT2_SPLIT_PATTERN); */
-  /* rt.train(text, 256 + 256, true); */
-  
-  /* return 0; */
+  if(train) {
+    cout << "Train!\n";
+  } else {
+    cout << "No Train!\n";
+  }
 
   using std::chrono::high_resolution_clock;
   using std::chrono::duration_cast;
   using std::chrono::milliseconds;
 
   auto t1 = high_resolution_clock::now(); // Record start time
-  auto verbose = true;
-  auto input = getTestString(2);
+  auto verbose = true; // TODO this should be an option
 
-  if(true) {
-    BasicTokenizer bt;
-    bt.train(input, 256 + 256, verbose);
-  }
+  /* if(true) { */
+  /*   BasicTokenizer bt; */
+  /*   bt.train(input, 256 + 256, verbose); */
+  /* } */
 
-  if(true) {
-    RegexTokenizer rt = RegexTokenizer(RegexTokenizer::GPT4_SPLIT_PATTERN);
-    rt.train(input, 256 + 256, verbose);
-  }
+  /* if(true) { */
+  /*   RegexTokenizer rt = RegexTokenizer(RegexTokenizer::GPT4_SPLIT_PATTERN); */
+  /*   rt.train(input, 256 + 256, verbose); */
+  /* } */
 
   auto t2 = high_resolution_clock::now(); // Record end time
   auto duration = t2 - t1;
---
+++ code/examples/train.cpp
@@ -0,0 +1,99 @@
+#include "BasicTokenizer.hpp"
+#include "RegexTokenizer.hpp"
+#include <fstream>
+#include <iostream>
+
+using std::string;
+using std::optional;
+
+// Roughly a port of https://github.com/karpathy/minbpe/blob/master/train.py
+
+// Note the default is to train on the taylorswift sample, but if you pass in an integer less
+// than the number of test strings I will run that one.
+// Each entry is either text to train on or if you prefix with FILE: it will load the file instead.
+const string test_strings[] = {
+  "FILE:data/taylorswift.txt",
+  "FILE:data/sample.txt",
+  "FILE:data/shakespeare.txt",
+  "FILE:data/bible.txt",
+  "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception.",
+  "", // empty string
+  "?", // single character
+  "안 👋 hello", // also from the youtube video
+  "hello world!!!? (안녕하세요!) lol123 😉", // fun small string
+  "But Unicode can be abstruse plus we know we out to be still finidng the whole thing mysterious",
+};
+
+string getTestString(int index) {
+  if (index < 0 || index >= sizeof(test_strings) / sizeof(test_strings[0])) {
+    std::cerr << "Index out of bounds." << std::endl;
+    return "";
+  }
+  string str = test_strings[index];
+  const string fileIndicator = "FILE:";
+  if (str.find(fileIndicator) != str.npos) {
+    std::string file_path = str.substr(fileIndicator.length());
+    std::ifstream file(file_path);
+    std::string file_content;
+
+    if (file) {
+      cout << "Loading input file: " << file_path << "\n";
+      file_content.assign((std::istreambuf_iterator<char>(file)),
+                          std::istreambuf_iterator<char>());
+      file.close();
+      return file_content;
+    } else {
+      std::cerr << "Could not open file: " << file_path << std::endl;
+      return "";
+    }
+  }
+  cout << str << " does not begin with " << fileIndicator << "\n";
+  return str;
+}
+
+int main(int argc, char *argv[]) {
+  using std::chrono::high_resolution_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::milliseconds;
+
+  long test_index = 0;
+  if (argc > 1) {
+    char *endptr;
+    long number = strtol(argv[1], &endptr, 10);
+
+    // If the entire argument was a valid number, print it
+    if (*endptr == '\0') {
+        long num_elements = sizeof(test_strings) / sizeof(test_strings[0]);
+        test_index = number < num_elements ? number : 0;
+    }
+  }
+
+  auto t1 = high_resolution_clock::now(); // Record start time
+  auto verbose = true;
+  auto input = getTestString(test_index);
+
+  int num_tokens = 512;
+
+  BasicTokenizer bt;
+  bt.train(input, num_tokens, verbose);
+
+  RegexTokenizer rt = RegexTokenizer(RegexTokenizer::GPT4_SPLIT_PATTERN);
+  rt.train(input, num_tokens, verbose);
+
+  auto t2 = high_resolution_clock::now(); // Record end time
+  auto duration = t2 - t1;
+  auto ms_int = duration_cast<milliseconds>(duration).count();
+
+  std::cout << "Execution time: " << ms_int / 1000.0 << " seconds" << std::endl;
+
+  // Can uncomment to display and verify the output
+  if(false) {
+    /* auto encoded = rt.encode(input, verbose); */
+    /* cout << "Original string length " << input.size() << "\n"; */
+    /* cout << "Encoded string length " << encoded.size() << "\n"; */
+    /* auto decoded = rt.decode(encoded, false); */
+    /* cout << "Decoded string length " << decoded.size() << "\n"; */
+
+    /* cout << "Original == decoded: " << (decoded == input ? "Yes" : "No") << "\n"; */ 
+  }
+}
