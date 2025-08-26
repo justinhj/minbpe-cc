aa1739bf8e883482cd07aa91df6be70475badfed
Justin Heyes-Jones <justinhj@gmail.com>
2025-08-26 09:14:07 -0700

    second stage verbose in end to end

--- endtoend-test.sh
+++ endtoend-test.sh
@@ -10,7 +10,7 @@ mkdir -p tests
 diff tests/sample-decoded-basic data/sample.txt
 
 # Run the gpt4 regex with special tokens test
-./zig-out/bin/minbpe-cc --train --input data/taylorswift.txt --special-tokens-path ./data/special1.txt --model-path tests/gpt4-model --vocab-size 512 --encoder gpt4 --conflict-resolution first
+./zig-out/bin/minbpe-cc --train --input data/taylorswift.txt --special-tokens-path ./data/special1.txt --model-path tests/gpt4-model --vocab-size 512 --encoder gpt4 --conflict-resolution first --verbose
 ./zig-out/bin/minbpe-cc --encode --input data/specialtokensample.txt --model-path tests/gpt4-model --output tests/sample-encoded-gpt4
 ./zig-out/bin/minbpe-cc --decode --input tests/sample-encoded-gpt4 --model-path tests/gpt4-model --output tests/sample-decoded-gpt4
 diff tests/sample-decoded-gpt4 data/specialtokensample.txt
