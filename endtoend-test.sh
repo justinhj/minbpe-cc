dc918503ce649410f96f8862174b2857a37d6883
Justin Heyes-Jones <justinhj@gmail.com>
2025-09-07 10:36:39 -0700

    use taylor for end to end

--- endtoend-test.sh
+++ endtoend-test.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p tests
 
 # Run the basic encoding test
-./zig-out/bin/minbpe-cc --train --input data/shakespeare.txt --model-path tests/basic-model --vocab-size 512 --encoder basic --conflict-resolution lexical
+./zig-out/bin/minbpe-cc --train --input data/taylorswift.txt --model-path tests/basic-model --vocab-size 512 --encoder basic --conflict-resolution lexical
 ./zig-out/bin/minbpe-cc --encode --input data/sample.txt --model-path tests/basic-model --output tests/sample-encoded-basic
 ./zig-out/bin/minbpe-cc --decode --input tests/sample-encoded-basic --model-path tests/basic-model --output tests/sample-decoded-basic
 diff tests/sample-decoded-basic data/sample.txt
